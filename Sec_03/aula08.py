c78e828bbb98cb7229d27b586eaab3b27ba35c97
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-08-14 21:40:40 -0300

    Continuação da primeira parte do curso

---
+++ Sec_03/aula08.py
@@ -0,0 +1,12 @@
+nome = 'Rafael Antonio'
+sobrenome = 'Rodigues da Rosa'
+idade = 34
+ano_nascimento = 1989
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('É maior de idade?: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
