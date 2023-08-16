a991f840d006a4cab08a58be3beac1c9e6e3c9af
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-08-16 00:36:43 -0300

    continuação 15_08_23

---
+++ Sec_03/aula12.py
@@ -0,0 +1,7 @@
+# Calculo do IMC
+nome = 'Rafael A.R da Rosa'
+altura = 1.75
+peso = 100
+imc = peso / (altura ** 2)
+
+print(f'{nome} tem {altura}, pesa {peso} com  IMC de {imc:.2f}')
---
+++ Sec_03/aula15.py
@@ -0,0 +1,9 @@
+# Usando a função input para coletar dados do usuário
+
+nome = input('qual seu nome? ')
+print(f'O seu nome é {nome}')
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print (f'A soma dos números é {numero_1 + numero_2}')
