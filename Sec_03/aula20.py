e5a47c4ed62a59d91361f813b096e31ebea631d3
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-08-16 21:52:01 -0300

    continuação do estudo

---
+++ Sec_03/aula20.py
@@ -0,0 +1,12 @@
+# compare dois valores e informe qual é o mairo
+#
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(
+        f'o Primeiro valor = {primeiro_valor} é mairo que o segundo valor = {segundo_valor}')
+else:
+    print(
+        f'o Segundo valor = {segundo_valor} é mairo que o primeiro valor = {primeiro_valor}')
